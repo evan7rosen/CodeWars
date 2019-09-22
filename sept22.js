9ed8ba80b121bec14e960fe6ef6a651c78403c9a
Evan Rosen <Evan@admins-MacBook-Pro.local>
2019-09-22 09:27:46 -0700

    sorting problem

--- sept22.js
+++ sept22.js
@@ -5,3 +5,15 @@
 const binaryArrayToNumber = arr => {
   return parseInt(arr.join(""), 2);
 };
+
+// Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+function descendingOrder(n) {
+  var digits = n
+    .toString()
+    .split("")
+    .map(Number);
+  return parseInt(digits.sort((a, b) => b - a).join(""));
+}
+
+descendingOrder(123456789);
