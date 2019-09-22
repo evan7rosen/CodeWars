3d8fbd124110ce1ef3899711f7d4edb74a076671
Evan Rosen <Evan@admins-MacBook-Pro.local>
2019-09-22 08:59:36 -0700

    completed tower problem

--- sept19.js
+++ sept19.js
@@ -4,3 +4,22 @@
 function expressionMatter(a, b, c) {
   return Math.max(a + b + c, a * (b + c), (a + b) * c, a * b * c);
 }
+
+// Build Tower by the following given argument:
+// number of floors (integer and always greater than 0).
+
+// Tower block is represented as *
+
+function towerBuilder(nFloors) {
+  let arr = [];
+  const star = "*";
+  const space = " ";
+  for (let i = 1; i < nFloors + 1; i++) {
+    arr.push(
+      space.repeat(nFloors - i) +
+        star.repeat(i + i - 1) +
+        space.repeat(nFloors - i)
+    );
+  }
+  return arr;
+}
